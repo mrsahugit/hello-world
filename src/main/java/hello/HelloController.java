09bcbb3933f08cbad95b16b9463aa7c15c56621a
Manas Sahu <manas_sahu77@hotmail.com>
2016-07-31 07:26:40 +0530

    BugFix
    Updated

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -17,8 +17,8 @@ public class HelloController {
 	 * 
 	 */
 	@RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+    	public String index() {
+        	return "Greetings from Spring Boot!";
+    	}
 
 }
